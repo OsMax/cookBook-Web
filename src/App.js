41615121783946df560a3af0ff740b564dced5bd
OsMax <www.osmax@gmail.com>
2024-08-15 16:46:32 +0300

    add routes and pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 // import logo from "./logo.svg";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage/HomePage";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>HELLOW WORLD</h1>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        {/* <Route path="catalog" element={<CarsPage />} />
+        <Route path="favorite" element={<FavoritePage />} /> */}
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
